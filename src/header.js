b8ea113b0d713fb229480292011cf0f6d563f052
amirul <zainsyedahmad63@gmail.com>
2023-10-20 02:30:44 +0800

    test

---
+++ src/header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './images/headerlogo.png';
+
+function Header() {
+  return (
+    <div className="header">
+      <div className='logo-container'>
+        <img src={logo} alt="headerlogo" className="logo" />
+      </div>
+      <div className="title-container">
+        <h1>James Pokemon Store</h1>
+      </div>
+      <div className="menu-container">
+        <div className="menu-item">Singles card</div>
+        <div className="menu-item">Graded cards</div>
+        <div className="menu-item">Booster boxes</div>
+        <div className="menu-item">Mystery packs</div>
+        <div className="menu-item">Contact</div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
