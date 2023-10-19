13df040ac83026633c35e2eb0f2ddcbab811e5a2
amirul <zainsyedahmad63@gmail.com>
2023-10-20 02:49:38 +0800

    add image

---
+++ src/card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Card({ name, description, stock, price, onAddToCart, image }) {
+  return (
+    <div className="card">
+      <img src={require(`./images/${image}`)} alt={name} className="card-image" />
+      <h2>{name}</h2>
+      <p>{description}</p>
+      <p>Stock: {stock}</p>
+      <p>Price: ${price}</p>
+      <button onClick={onAddToCart}>Add to Cart</button>
+    </div>
+  );
+}
+
+export default Card;
