b7030dc48aa281b89d72b00e2c047a21dc7735d1
amirul <zainsyedahmad63@gmail.com>
2023-10-20 03:39:44 +0800

    test3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './header';
+import Card from './card';
+import './styles.css'; 
+
+const cardsData = [
+  // Data for 5 cards
+  { name: 'Charizard VMAX', description: 'Sword and Shield', stock: 10, price: 5.99, image: './images/charizardvmax.png'},
+  { name: 'Zapdos AR', description: 'Sword and Shield', stock: 8, price: 7.99, image: './images/galarianbird.png'},
+  { name: 'Mew', description: 'Sword and Shield', stock: 10, price: 5.99, image: './images/mew.png'},
+  { name: 'Pikachu', description: 'Sword and Shield', stock: 8, price: 7.99, image: './images/pikachu.png'},
+  { name: 'Arceus V', description: 'Sword and Shield', stock: 10, price: 5.99, image: './images/arceusv.png'},
+  { name: 'Umbreon VMAX', description: 'Sword and Shield', stock: 8, price: 7.99, image: './images/umbreonvmax.png'},
+  
+];
 
 function App() {
+  const handleAddToCart = () => {
+    // Handle adding to cart logic here
+    console.log('Added to cart!');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="card-container">
+        {cardsData.map((card, index) => (
+          <Card
+            key={index}
+            name={card.name}
+            description={card.description}
+            stock={card.stock}
+            price={card.price}
+            onAddToCart={handleAddToCart}
+            image={card.image}
+          />
+        ))}
+      </div>
     </div>
   );
 }
