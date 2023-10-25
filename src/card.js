d2d054bc58cb2822defcf36dcf2de79e36910daf
amirul <zainsyedahmad63@gmail.com>
2023-10-25 21:16:21 +0800

    card container rearrange

--- src/card.js
+++ src/card.js
@@ -5,12 +5,20 @@ function Card({ name, description, stock, price, onAddToCart, image }) {
   return (
     <div className="card">
       <img src={require(`${image}`)} alt={name} className="card-image" />
-      <div className='card-details'>
+      <div className="card-details">
         <h2>{name}</h2>
-        <p className="price">${price}</p>
-        <p className="description">{description}</p>
-        <p className="stock">Stock: {stock}</p>
-        <button className="addtocartbtn" onClick={onAddToCart}>Add to Cart</button>
+        <div className="price-description-stock-container">
+          <div className="description-stock-container">
+            <p className="description">{description}</p>
+            <p className="stock">Stock: {stock}</p>
+          </div>
+          <div className="price-container">
+            <p className="price">${price}</p>
+          </div>
+        </div>
+        <button className="addtocartbtn" onClick={onAddToCart}>
+          Add to Cart
+        </button>
       </div>
     </div>
   );
