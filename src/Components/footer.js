30b7eec03a782af0e0c59e837701fec60c943ae8
amirul <zainsyedahmad63@gmail.com>
2023-11-07 01:58:51 +0800

    style footer 2

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -3,6 +3,11 @@ import cartIcon from '../icons/cart.png';
 import '../styles-footer.css'
 
 function Footer() {
+  const ChatText = "Available Monday - Friday (8:00am - 5:00pm PST) Quickest way to get your questions answered";
+  const locationText = "Email us at jamestore@gmail.com. We’ll get to you as soon as possible";
+  const emailusText = "James Store Marketplace 7560 SW Durham Rd Tigard, OR 97224";
+  const moreText = "PRIVACY POLICY \n STORE POLICY \n RETURN POLICY \n ABOUT";
+
   return (
     <div className="footer-container">
       <div className="footer-title-container">
@@ -10,19 +15,31 @@ function Footer() {
       </div>
       <div className="footer-columns">
         <div className="footer-column">
-          <p className="footer-column-title">Chat</p>
+          <p className="footer-column-title">CHAT</p>
+          <div className="footer-column-description-container">
+            <p className="footer-column-description">{ChatText}</p>
+          </div>
         </div>
         <div className="footer-column">
-          <p className="footer-column-title">Location</p>
+          <p className="footer-column-title">LOCATION</p>
+          <div className="footer-column-description-container">
+            <p className="footer-column-description">{locationText}</p>
+          </div>
         </div>
         <div className="footer-column">
-          <p className="footer-column-title">Email Us</p>
+          <p className="footer-column-title">EMAIL US</p>
+          <div className="footer-column-description-container">
+            <p className="footer-column-description">{emailusText}</p>
+          </div>
         </div>
         <div className="footer-column">
-          <p className="footer-column-title">More</p>
+          <p className="footer-column-title">MORE</p>
+          <div className="footer-column-description-container">
+            <p className="footer-column-description">{moreText}</p>
+          </div>
         </div>
       </div>
-      <div className="social-icons">
+      <div className="social-icons-container">
         <img src={cartIcon} alt="Cart" className="icon" />
         <img src={cartIcon} alt="Cart" className="icon" />
         <img src={cartIcon} alt="Cart" className="icon" />
